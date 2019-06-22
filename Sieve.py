d7d73b74ea707702fb79d94890111a1e82b35069
eriktoger <erik.toger@gmail.com>
2019-06-22 09:27:20 +0200

    Sieve of Eratosthenes

---
+++ Sieve.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 20 20:53:22 2019
+
+@author: erik
+"""
+
+upperLimit = 10**4
+primeList = list( range(2,upperLimit+1))
+
+primeIndex = 0;
+while(primeIndex < len(primeList)):
+    currentPrime = primeList[primeIndex]
+    sieveNr = currentPrime*2
+    compsiteNrSet = set()
+    
+    while(sieveNr <= upperLimit):
+        compsiteNrSet.add(sieveNr)
+        sieveNr += currentPrime
+    
+    primeList = list ( set(primeList) - compsiteNrSet)
+    primeList.sort()
+    primeIndex = primeIndex + 1
+    
+
+print(len(primeList) )
+    
+    
+    
+    
+    
